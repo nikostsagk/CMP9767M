391f2af9317b7463ff580e2332b56adab39afcb1
nikostsagk <n.tsagko@gmail.com>
2021-10-19 15:34:27 +0100

    working thorvald_controller

--- uol_cmp9767m_tutorial/scripts/thorvald_controller.py
+++ uol_cmp9767m_tutorial/scripts/thorvald_controller.py
@@ -1,61 +1,77 @@
 #!/usr/bin/env python
 
-import sys
-from pynput import keyboard
+import random
+import numpy as np
 
 import rospy
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 
-def on_shutdown():
-    rospy.loginfo("Shutting down...")
 
 class ThorvaldController:
     def __init__(self):
         self.velocity_pub = rospy.Publisher("/thorvald_001/twist_mux/cmd_vel", Twist, queue_size=1)
-    
+        self.lidar_sub = rospy.Subscriber("/thorvald_001/front_scan", LaserScan, self.lidar_cb, queue_size=1)
+        self.odometry_sub = rospy.Subscriber("/thorvald_001/odometry/base_raw", Odometry, self.odom_cb, queue_size=1)
+        self.mileage_pub = rospy.Publisher("/mileage", String, queue_size=1)
+
+        self.just_spawned = True
+        self.mileage = 0
+
+    def lidar_cb(self, data):
+        # TODO: Merge the 2 lidars to a 360degree one, and use
+        # the index of the beam that faces the front of the robot.
+        min_dist = min(data.ranges)
+        if min_dist > 3:
+            self.go_forward()
+        else:
+            self.go_left()
+
+    def odom_cb(self, data):
+        if self.just_spawned == True:
+            self.old_x = data.pose.pose.position.x
+            self.old_y = data.pose.pose.position.y
+            self.just_spawned = False
+
+        x = data.pose.pose.position.x
+        y = data.pose.pose.position.y
+
+        mileage = np.sqrt((x - self.old_x)**2 + (y - self.old_y)**2)
+        self.mileage += mileage
+
+        self.mileage_pub.publish("Miles driven: {:.3f}".format(self.mileage/1609))
+        self.old_x = data.pose.pose.position.x
+        self.old_y = data.pose.pose.position.y
+
+
     def go_forward(self):
         msg = Twist()
-        msg.linear.x = 2
+        msg.linear.x = 0.8
         self.velocity_pub.publish(msg)
-    
+
+    def go_back(self):
+        msg = Twist()
+        msg.linear.x = -0.8
+        self.velocity_pub.publish(msg)
+
     def go_left(self):
         msg = Twist()
-        msg.angular.z = 0.1
+        msg.angular.z = random.uniform(0, 3.14)
         self.velocity_pub.publish(msg)
-    
-    def go_right(self):
-        msg = Twist()
-        msg.angular.z = -0.1
-        self.velocity_pub.publish(msg)
-    
+
     def stop(self, key):
         msg = Twist()
         self.velocity_pub.publish(msg)
-    
-    def move(self, key):
-        try:
-            if key == keyboard.KeyCode(char="w"):
-                self.go_forward()
-            elif key == keyboard.KeyCode(char="a"):
-                self.go_left()
-            elif key == keyboard.KeyCode(char="d"):
-                self.go_right()
-            elif key == keyboard.KeyCode(char="q"): # Doesn't work
-                rospy.signal_shutdown("Shutting down")
-                sys.exit()
-        except AttributeError:
-            pass
+
 
 def main():
     tc = ThorvaldController()
-    with keyboard.Listener(
-        on_press=tc.move,
-        on_release=tc.stop) as listener:
-        listener.join()
 
 if __name__ == "__main__":
     try:
-        rospy.init_node("thorvald_controller", anonymous=True, disable_signals=True)
+        rospy.init_node("thorvald_controller", anonymous=True)
         main()
         rospy.spin()
     except rospy.ROSInterruptException:
